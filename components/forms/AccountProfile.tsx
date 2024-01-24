9b502057acbe3da703a53d77cceaeba5683dbe82
Gabo <gabo@Mac-mini-de-Gabo.local>
2024-01-24 00:06:25 -0600

    Added uploadthing, finished Account profile component

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -17,7 +17,9 @@ import { Input } from "@/components/ui/input"
 import { Textarea } from "@/components/ui/textarea"
 import * as z from 'zod';
 import Image from 'next/image';
-import { ChangeEvent } from 'react';
+import { ChangeEvent, useState } from 'react';
+import { isBase64Image } from '@/lib/utils';
+import { useUploadThing } from '@/lib/uploadthing';
 
 interface Props {
     user: {
@@ -33,6 +35,9 @@ interface Props {
 
 const AccountProfile = ({ user, btnTitle }: Props) => {
 
+    const [files, setFiles] = useState<File[]>([]);
+    const { startUpload } = useUploadThing("media");
+
     const form = useForm({
         resolver: zodResolver(UserValidation),
         defaultValues: {
@@ -42,19 +47,48 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
             bio:  user?.bio || '',
         }
     });
-    const handleImage = (e: ChangeEvent, fieldChange:(value:string) => void ) => {
+    const handleImage = (e: ChangeEvent<HTMLInputElement>, fieldChange:(value:string) => void ) => {
         e.preventDefault();
+
+        const fileReader = new FileReader();
+
+        if(e.target.files && e.target.files.length > 0){
+            const file = e.target.files[0];
+            setFiles(Array.from(e.target.files));
+
+            if(!file.type.includes('image')) return;
+
+            fileReader.onload = async (event) => {
+                const imageDataUrl = event.target?.result?.toString() || '';
+
+                fieldChange(imageDataUrl);
+            }
+
+            fileReader.readAsDataURL(file);
+
+        }
     }
-    function onSubmit(values: z.infer<typeof UserValidation>) {
-        // Do something with the form values.
-        // ✅ This will be type-safe and validated.
-        
+    const onSubmit = async (values: z.infer<typeof UserValidation>) => {
+        const blob = values.profile_photo;
+
+
+        const hasImageChanged = isBase64Image(blob);
+
+        if(hasImageChanged){
+            const imgRes = await startUpload(files)
+
+            if(imgRes && imgRes[0].fileUrl) {
+                values.profile_photo = imgRes[0].fileUrl;
+            }
+        }
     }
+    
     return(
         <Form {...form}>
             <form 
-            onSubmit={form.handleSubmit(onSubmit)} 
-            className="flex flex-col justify-start-gap-10">
+            className="flex flex-col justify-start-gap-10"
+            onSubmit={form.handleSubmit(onSubmit)}
+            >
                 <FormField
                     control={form.control}
                     name="profile_photo"
@@ -98,7 +132,7 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
                     name="name"
                     render={({ field }) => (
                         <FormItem className='flex flex-col w-full gap-3'>
-                            <FormLabel className='text-base-semibold text-light-2'>
+                            <FormLabel className='text-base-semibold text-light-2 mt-4'>
                                 Name
                             </FormLabel>
                             <FormControl className='flex-1 text-base-semibold text-gray-200'>
@@ -116,7 +150,7 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
                     control={form.control}
                     name="username"
                     render={({ field }) => (
-                        <FormItem className='flex flex-col w-full gap-3'>
+                        <FormItem className='flex flex-col w-full gap-3 mt-4'>
                             <FormLabel className='text-base-semibold text-light-2'>
                                 Username
                             </FormLabel>
@@ -136,7 +170,7 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
                     name="bio"
                     render={({ field }) => (
                         <FormItem className='flex flex-col w-full gap-3'>
-                            <FormLabel className='text-base-semibold text-light-2'>
+                            <FormLabel className='text-base-semibold text-light-2 mt-4'>
                                 Bio
                             </FormLabel>
                             <FormControl className='flex-1 text-base-semibold text-gray-200'>
@@ -150,10 +184,10 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
                     )}
                 />
                 
-                <Button type="submit" className='bg-primary-500'>Submit</Button>
+                <Button type="submit" className='bg-primary-500 mt-4'>Submit</Button>
             </form>
         </Form>
-    )
-}
+    );
+};
 
 export default AccountProfile;
