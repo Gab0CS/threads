d63e392e79e19dfff17bf3d74ab0c8aaffee1301
Gab0CS <gabocanseco@gmail.com>
2024-01-29 22:43:03 -0600

    ThreadCard finished, logic implemented to manage threads in the home

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -11,6 +11,7 @@ interface Params {
     communityId: string | null,
     path: string,
 }
+
 export async function createThread({ text, author, communityId, path}: Params) {
     try {
         connectToDB();
@@ -32,4 +33,35 @@ export async function createThread({ text, author, communityId, path}: Params) {
     }
     
     
+}
+
+export async function fetchPosts(pageNumber = 1, pageSize= 20){
+    connectToDB();
+
+    //Calculate de number of post to skip
+    const skipAmount = (pageNumber - 1) * pageSize;
+
+    // To fetch only top level threads
+    const postsQuery =  Thread.find({ parentId: { $in: [null, undefined]} })
+    .sort({ createdAt: 'desc' })
+    .skip(skipAmount)
+    .limit(pageSize)
+    .populate({ path: 'author', model: User })
+    .populate({ 
+        path: 'children',
+        populate: {
+            path: 'author',
+            model: User,
+            select: "_id name parentId Image"
+        }
+    })
+
+    const totalPostsCount =  await Thread.countDocuments({ parentId: { $in: 
+    [null, undefined]} });
+
+    const posts = await postsQuery.exec();
+
+    const isNext = totalPostsCount > skipAmount + posts.length;
+
+    return { posts, isNext }
 }
