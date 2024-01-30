dcdf2a6abe91e9a05b192e978ef79c703fe0394e
Gabo <gabo@Mac-mini-de-Gabo.local>
2024-01-30 00:25:08 -0600

    Started logic for Comments

--- lib/validations/thread.ts
+++ lib/validations/thread.ts
@@ -6,7 +6,7 @@ export const ThreadValidation = z.object({
     accountId: z.string(),
 })
 
-export const ComentValidation = z.object({
+export const CommentValidation = z.object({
     thread: z.string().nonempty().min(3, { message: 
     'Minimun 3 characters'}),
     accountId: z.string(),
